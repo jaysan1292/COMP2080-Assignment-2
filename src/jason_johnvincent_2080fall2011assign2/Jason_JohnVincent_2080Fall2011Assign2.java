56b309e99c170129f62835455682ac2fe24f839d
Jason Recillo <jaysan1292@gmail.com>
2011-11-16 19:44:32 -0500

    init

---
+++ src/jason_johnvincent_2080fall2011assign2/Jason_JohnVincent_2080Fall2011Assign2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jason_johnvincent_2080fall2011assign2;
+
+/**
+ *
+ * @author Jason Recillo
+ */
+public class Jason_JohnVincent_2080Fall2011Assign2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
